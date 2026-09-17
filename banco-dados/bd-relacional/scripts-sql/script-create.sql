09e9dd11318d5de3e3f2bd8b3a0918b6c493eaa0
Matheus Pereira Rodrigues <mts027123@gmail.com>
2026-09-17 11:04:26 -0300

    Finaliza implementação do cadastro cliente e corrige script create

--- banco-dados/bd-relacional/scripts-sql/script-create.sql
+++ banco-dados/bd-relacional/scripts-sql/script-create.sql
@@ -132,7 +132,7 @@ CREATE TABLE Conjuge (
     data_nascimento DATE,
     url_comprovante_uniao VARCHAR(255) NOT NULL,
     data_casamento DATE NOT NULL,
-    casamento_ativo ENUM('sim', 'nao'),
+    casamento_ativo ENUM('sim', 'nao') NOT NULL,
     data_fim_casamento DATE,
     id_cliente INTEGER UNSIGNED,
     PRIMARY KEY (id_cliente, cpf)
